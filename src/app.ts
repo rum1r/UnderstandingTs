e5897009d2f4fb764e7aa1915b4befc60566c9a9
Rumiko Ueno <momotarouko@gmail.com>
2022-01-04 09:25:30 +0900

    128. シングルトン & アプリケーションの状態管理

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,51 @@
+
+/**
+ * Project State Management
+ */
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {
+
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+  /**
+   * プロジェクト追加時
+   * @param title 
+   * @param description 
+   * @param manday 
+   */
+  addProject(title: string, description: string, manday: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      manday: manday,
+    }
+    this.projects.push(newProject);
+    // 登録された関数をすべて実行
+    for (const listenerFn of this.listeners) {
+      // `slice`はコピーを渡す意味 関数内で変更させないため
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 // Validation
 interface Validatable {
   value: string | number;
@@ -54,39 +102,71 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   };
   return adjDescriptor;
 }
-// ProjectList Class
+
+/**
+ * ProjectList Class
+ */
 class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
+  assignedProjects: any[];
 
+  /**
+   * constructor
+   * 
+   * @param type 
+   */
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
       "project-list"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.assignedProjects = [];
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+
+    // プロジェクト追加時実行する関数の登録
+    projectState.addListener((projects : any[]) => {
+      this.assignedProjects = projects;
+      this.renderProjects();
+    })
+
     this.attach();
     this.renderContent();
   }
-
-  private attach(){
-    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  /**
+   * プロジェクト追加時実行される関数
+   */
+  private renderProjects() {
+    const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement('li');
+      listItem.textContent = prjItem.title;
+      listEl.appendChild(listItem);
+    }
   }
+
   private renderContent() {
     const listId = `${this.type}-project-list`;
     this.element.querySelector('ul')!.id = listId;
     this.element.querySelector('h2')!.textContent = this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
 
   }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+
 }
 
-// ProjectInput Class
+/**
+ * ProjectInput Class
+ */
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -167,6 +247,8 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, manday] = userInput;
+      // プロジェクト追加時 addProject
+      projectState.addProject(title, desc, manday);
       console.log(title, desc, manday);
       this.clearInputs();
     }
